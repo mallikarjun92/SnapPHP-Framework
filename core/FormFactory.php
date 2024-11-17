2ac1e355aa90f3dc44cbd0f656514d7d368510ee
Mallikarjun <mallikarjun016.rymec@gmail.com>
2024-11-17 18:54:57 +0530

    new update - integrated symfony/form

---
+++ core/FormFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace SnapPHP\Core;
+
+use Symfony\Component\Form\Forms;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Security\Csrf\CsrfTokenManager;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Form\Extension\Csrf\CsrfExtension;
+
+class FormFactory
+{
+    private static ?FormFactoryInterface $factory = null;
+
+    public static function getFactory(): FormFactoryInterface
+    {
+        if (self::$factory === null) {
+            $csrfTokenManager = new CsrfTokenManager();
+            $validator = Validation::createValidator();
+
+            self::$factory = Forms::createFormFactoryBuilder()
+                ->addExtension(new \Symfony\Component\Form\Extension\Validator\ValidatorExtension($validator))
+                ->addExtension(new CsrfExtension($csrfTokenManager))
+                ->getFormFactory();
+        }
+        return self::$factory;
+    }
+}
