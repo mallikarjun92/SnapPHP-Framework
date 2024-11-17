94010d995126f5218ed95e6f8aebb744b35ed6cc
Mallikarjun <mallikarjun016.rymec@gmail.com>
2024-11-17 19:48:30 +0530

    new update forms

--- core/FormFactory.php
+++ core/FormFactory.php
@@ -4,8 +4,6 @@ namespace SnapPHP\Core;
 
 use Symfony\Component\Form\Forms;
 use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\Form\RequestHandlerInterface;
-use Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationRequestHandler;
 use Symfony\Component\Validator\Validation;
 use Symfony\Component\Security\Csrf\CsrfTokenManager;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
@@ -20,12 +18,10 @@ class FormFactory
         if (self::$factory === null) {
             $csrfTokenManager = new CsrfTokenManager();
             $validator = Validation::createValidator();
-            $requestHandler = new HttpFoundationRequestHandler();
 
             self::$factory = Forms::createFormFactoryBuilder()
                 ->addExtension(new \Symfony\Component\Form\Extension\Validator\ValidatorExtension($validator))
                 ->addExtension(new CsrfExtension($csrfTokenManager))
-                ->setRequestHandler($requestHandler)
                 ->getFormFactory();
         }
         return self::$factory;
